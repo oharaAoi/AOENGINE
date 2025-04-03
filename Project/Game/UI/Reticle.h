e5b3cff5c87322a244f414e5e69967a5d18b3997
oharaAoi <k023g0108@g.neec.ac.jp>
2025-04-03 14:13:08 +0900

    reticleクラスの追加

--- Project/Game/UI/Reticle.h
+++ Project/Game/UI/Reticle.h
@@ -1,4 +1,20 @@
 #pragma once
+#include <memory>
+#include "Engine/Components/2d/Sprite.h"
+
 class Reticle {
+public:
+
+	Reticle() = default;
+	~Reticle() = default;
+
+	void Init();
+	void Update();
+	void Draw() const;
+
+private:
+
+	std::unique_ptr<Sprite> reticle_;
+
 };
 
