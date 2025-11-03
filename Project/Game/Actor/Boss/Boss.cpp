2562b25baee8e0a0ccd0bf87f5db01b5f65b552f
oharaAoi <k023g0108@g.neec.ac.jp>
2025-11-03 18:02:41 +0900

    ボスを止めて置く

--- Project/Game/Actor/Boss/Boss.cpp
+++ Project/Game/Actor/Boss/Boss.cpp
@@ -123,7 +123,7 @@ void Boss::Init() {
 	behaviorTree_->AddCanTask(CreateTask<BossActionRapidfire>(this, "Rapidfire"));
 	behaviorTree_->AddCanTask(CreateTask<BossActionAdjustHeight>(this, "AdjustHeight"));
 	behaviorTree_->CreateTree("./Project/Packages/Game/Assets/GameData/BehaviorTree/BossTree.json");
-	behaviorTree_->SetExecute(true);
+	behaviorTree_->SetExecute(false);
 
 	evaluationFormula_ = std::make_unique<BossEvaluationFormula>();
 	evaluationFormula_->Init(this);
