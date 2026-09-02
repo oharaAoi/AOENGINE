4bfe0dad662eb5db9095fa994a905b11a1ade679
ku-rihara <k023g0027@g.neec.ac.jp>
2026-09-02 23:49:22 +0900

    a

---
+++ Project/Game/Stage/StageFactory.h
@@ -0,0 +1,20 @@
+#pragma once
+
+// game
+#include "Game/Stage/StageBlockField.h"
+#include "Game/Stage/StageSegment.h"
+
+/// <summary>
+/// ステージ自動を生産するためのクラス
+/// </summary>
+class StageFactory {
+public:
+	/// <summary>
+	/// 生成関数
+	/// </summary>
+	/// <param name="field">セグメントを跨いでブロックを管理する連結グループ表</param>
+	/// <param name="index">このセグメントの原点となるグローバルグリッドX座標</param>
+	/// <param name="addHeight">追加される座標の高さ</param>
+	void Create(StageBlockField* field, StageSegment*segment, int index, int addHeight);
+};
+
