4ca3aa3ea7810f8bc61ee62a0c662dea364d2202
OriharaIssei <k023g0048@g.neec.ac.jp>
2026-09-02 11:34:48 +0900

    とりあえず、読み込みまで。ワールドへの配置がまだ

---
+++ Project/Game/Stage/StageSegment.cpp
@@ -0,0 +1,16 @@
+#include "StageSegment.h"
+
+/// externals
+#include "rapidcsv/rapidcsv.h"
+
+void StageSegment::LoadBlockData(const std::string& filePath){
+	// CSVからブロックの配置データを読み込む
+	// ラベル行・列は無しで読み込む (－1,ー1 でラベルを無効化)
+	rapidcsv::Document doc(filePath, rapidcsv::LabelParams(-1, -1));
+
+	for (int i = 0; i < kBlockRow; ++i) {
+		for (int j = 0; j < kBlockCol; ++j) {
+			blockData_[i][j] = doc.GetCell<int>(j, i);
+		}
+	}
+}
